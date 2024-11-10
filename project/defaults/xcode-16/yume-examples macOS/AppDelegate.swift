b1e61e637232d42d5ffa1995f62337a024dd5973
raina-reina <canine_pangs0e@icloud.com>
2024-11-10 11:35:41 -0800

    Added default Xcode 16 Metal game project

---
+++ project/defaults/xcode-16/yume-examples macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  yume-examples macOS
+//
+//  Created by Raina on 11/10/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
